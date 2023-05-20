e3e393be12316546506c9a764ada82eb478fe7b1
Leonardo Pinto Passeri <leoppasseri@gmail.com>
2023-05-19 22:36:40 -0300

    att

--- Linguagem C/LISTA7 - 2023.1/ex2.c
+++ Linguagem C/LISTA7 - 2023.1/ex2.c
@@ -1,4 +1,4 @@
-int analizador(int vetor[],int qtd,int *pares,int *impares){
+void analizador(int vetor[],int qtd,int *pares,int *impares){
 
     for(int c = 0;c < qtd;c++){
         if(vetor[c] % 2 == 0){
