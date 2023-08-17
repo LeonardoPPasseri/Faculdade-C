85c9636e1d0843bb7536576fea400f1497956fe8
Leonardo Pinto Passeri <leoppasseri@gmail.com>
2023-08-17 10:42:10 -0300

    att

--- Criação.c
+++ Criação.c
@@ -1,44 +1,103 @@
 #include <stdio.h>
-#define MAX 4
-void Ordenador(int vetor[]){
-    int aux,j;
-    for(int i=1;i<MAX;i++){
-        if(vetor[i]<0){
-            aux = vetor[i];
-            j = i-1;
-            while(j >= 0 && aux > vetor[j]){
-                vetor[j+1]=vetor[j];
-                j--;
+#include <time.h>
+#include <stdlib.h>
+
+void Merge_Sort(int vetor[],int aux[],int TAM,int ini,int fim){
+    int i,j,k;
+    if(ini < fim){
+        int meio = (ini+fim)/2;
+        Merge_Sort(vetor,aux,TAM,ini,meio);
+        Merge_Sort(vetor,aux,TAM,meio+1,fim);
+
+        i = ini;
+        j = meio+1;
+        k = ini;
+// 5 4 3 2 
+// ini = (0)
+// fim = (3)
+// meio = (1)
+// meio + 1 = (2)
+//1mergesort[1](0 1)
+//ini=0
+//fim=1
+//ini+fim/2 ==> 0
+//meio=0
+//2mergesort[1] (0 0)
+//IF da negativo e a recursividade desmonta a pilha voltando para o merge_sort[2]
+//meio+1=2
+//fim=3
+//mergesort[2](2 3)
+//ini=2
+//fim=3
+//ini+fim/2 ==> 2
+//mergesort[1] (2 2)
+//if da negativo e a recursividade desmonta a pilha voltando para o merge_sort[2]
+//if da negativo e a recursividade desmonta a pilha começando a usar o WHILE
+//ini=
+
+        //MERGE
+        while(i <= meio && j <= fim){
+            if(vetor[i] < vetor[j]){
+                aux[k] = vetor[i];
+                i++;
+                k++;
             }
-            vetor[j+1]=aux;
-        }else{
-            aux = vetor[i];
-            j = i-1;
-            while(j >= 0 && aux < vetor[j]){
-                vetor[j+1]=vetor[j];
-                j--;
+            else{
+                aux[k] = vetor[j];
+                j++;
+                k++;
             }
-            vetor[j+1]=aux;
+        }
+        while(i <= meio){
+            aux[k] = vetor[i];
+            i++;
+            k++;
+        }
+        while(j <= fim){
+            aux[k]=vetor[j];
+            j++;
+            k++;
+        }
+        //Copiando o vetor aux para o vetor original
+        for(int c=ini;c<=fim;c++){
+            vetor[c] = aux[c];
         }
     }
-}
-//2 4 -2 0
-//2 4 -2 0
-//
-
-void inicializador(int vetor[]){
-    for(int c=0;c<MAX;c++){
-        vetor[c]=0;
+    printf("\ninicio:%d, fim:%d  :",ini,fim);
+    for(int c=0;c<TAM;c++){
+        printf("%d ",vetor[c]);
     }
+    printf("\n");
+    for(int c=0;c<TAM;c++){
+        printf("%d ",aux[c]);
+    }
+}
+
+
+void imprimir(int vetor[],int TAM){
+    printf("\nVetor Ordenado: \n");
+    for(int c=0;c<TAM;c++){
+        printf("%d ",vetor[c]);
+    }
+}
+void aleatorio(int vetor[], int TAM){
+    srand(time(NULL));
+    printf("\nValores do vetor: \n");
+    for(int c=0;c<TAM;c++){
+        vetor[c] = ((rand() - (RAND_MAX/2))%500);
+        printf("%d ",vetor[c]);
+    }
+    printf("\n\n");
 }
 
 int main(){
-    int vetor[MAX];
-    inicializador(vetor);
-    printf("Digite os numeros: ");
-    for(int c=0;c<MAX;c++){
-        scanf("%d",&vetor[c]);
-    }
-    Ordenador(vetor);
+    int vetor[8];
+    int TAM = sizeof(vetor)/sizeof(vetor[0]);
+    int aux[8] = {0};
+    aleatorio(vetor,TAM);
+    int ini = 0;
+    int fim = TAM-1;
+    Merge_Sort(vetor,aux,TAM,ini,fim);
+    imprimir(vetor,TAM);
     return 0;
 }
